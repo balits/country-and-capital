645bfa3958685366200a60dbfd454f6b6458ef93
Balits Peter <97095806+balits@users.noreply.github.com>
2022-04-26 20:08:35 +0200

    Just realized I make such big commits, that no short message would explain the progress

--- main.js
+++ main.js
@@ -1,18 +1,53 @@
+const colors = {
+  'easy': 'bg-green-500',
+  'medium': 'bg-yellow-400',
+  'hard': 'bg-red-500',
+  'bg': 'bg-gray-300',
+  'textLight': 'text-gray-500',
+  'textDark': 'text-slate-900',
+  'goodGuess' : 'bg-green-300',
+  'badGuess' : 'bg-red-300',
+}
+
 // GLOBAL VARIABLES
 let list = [];
-let used = [];
 let difficulty = document.getElementById("easy").id;
+
 let gameIsRunnig = false;
+
 const secretField = document.getElementById("country");
 const guessField = document.getElementById("capital");
-const led = document.getElementById("led");
+const startButton = document.getElementById('start');
+const resetButton = document.getElementById('reset');
+const led = document.getElementById("background");
+
+startButton.addEventListener('click',() => {
+  if (!gameIsRunnig && startButton.classList.contains('cursor-pointer')) {
+    start();
+    
+    startButton.classList.remove('cursor-pointer')
+    startButton.classList.add('cursor-not-allowed')
+  }
+});
+
+resetButton.addEventListener('click', () => {
+  guessField.value = "";
+  if (gameIsRunnig) {
+    gameIsRunnig = false;
+    alert("GAME SHOULD BE RUNNING");
+    // window.location.reload();
+  }
+});
+
 
 // fetching data everytime the page is refreshed
-// instead of everytime a new game begins
 fetch("https://restcountries.com/v2/all")
 .then((res) => res.json())
 .then((data) => initData(data))
 .catch((err) => alert(err));
+setTimeout(() =>{}, 900);
+
+
 // extract and savedata from API
 function initData(d) { list = d; }
 
@@ -20,73 +55,54 @@ function initData(d) { list = d; }
 function selectDiff() {
   const currentType = event.target;
   difficulty = currentType.id;
-
-  let types = document.getElementsByClassName("mode");
-  let activeType = undefined;
-  let allInactive = true;
-
+  
+  let types = document.getElementById('modes').children;
   for (let i = 0; i < types.length; i++) {
-    if (!types[i].classList.contains("hide-color")) {
-      allInactive = false;
-      activeType = types[i];
-      break;
+    if (types[i] == currentType) {
+      if (types[i].classList.contains( colors.bg )) {
+        types[i].classList.remove( colors.bg )
+        types[i].classList.remove( colors.textLight )
+      }
+      types[i].classList.add( colors[types[i].id] );
+      types[i].classList.add( colors.textDark );
+      console.log(types[i].classList);
     }
-  }
+    else {
+      if (!types[i].classList.contains( colors.bg )) {
+        types[i].classList.remove( colors[types[i].id] )
+        types[i].classList.remove( colors.textDark )
 
-  if (activeType === currentType) {
-    return activeType.id;
-  }
-
-  if (allInactive) {
-    currentType.classList.add(currentType.id);
-    currentType.classList.remove("hide-color");
-  } else {
-    activeType.classList.remove(activeType.id);
-    activeType.classList.add("hide-color");
-
-    currentType.classList.remove("hide-color");
-    currentType.classList.add(currentType.id);
-  }
+        types[i].classList.add( colors.bg )
+        types[i].classList.add( colors.textLight )
+      }
+    }
+  }  
 }
-
-
 
 // create new list according to the difficulty selected
 function generateList() {
   let l = [];
-  // easy    56 countries
-  // medium  __ countries
-  // hard    __ countries
-
+  
   switch (difficulty) {
-     case "easy":
+    case "easy":
       list.forEach((e) => {
         if (!l.includes(e)) {
-          if (
-            (e.subregion === "Northern America" && e.population >= 5000000) ||
-            (e.region === "Europe" && e.population >= 5000000) ||
-            (e.population >= 100000000)
-          ) {
-            l.push(e);
+          if ( (e.subregion === "Northern America" && e.population >= 5000000) || (e.region === "Europe" && e.population >= 5000000) || (e.population >= 100000000)) {
+              l.push(e);
           }
         }
       });
-      break;
+    break;
 
     // needs refinement
     case "medium":
       list.forEach((e) => {
-        if (
-          !list.includes(e) ||
-          (e.population <= 400000000 && e.population >= 1000000) ||
-          e.region === "America" ||
-          e.subregion === "Southern Asia"
-        ) {
-          l.push(e);
+        if (!list.includes(e) || (e.population <= 400000000 && e.population >= 1000000) || e.region === "America" || e.subregion === "Southern Asia") {
+            l.push(e);
         }
       });
-      break;
-
+    break;
+            
     // needs refinement
     case "hard":
       list.forEach((e) => {
@@ -94,70 +110,68 @@ function generateList() {
           l.push(e);
         }
       });
-      break;
-
+    break;
+      
     default:
-      break;
+    break;
   }
 
   return l;
 }
-
+            
 // initiate new game (by pressing 'START')
-function startGame() {
-  if (difficulty === undefined) {
-    alert("Select a difficulty!");
-    return;
-  }
-  // this makes it so that you cannot start
-  // X amount of games, since that seemingly
-  // creates unexpected behavior
-  let startButton = document.getElementsByClassName('start-btn')
-  startButton[0].onclick = null
-  startButton[0].style.cursor = 'not-allowed'
-
+function start() {
   console.log("NEW GAME STARTED\tMODE: " + difficulty);
-
-  setTimeout(() => {
-    let listAsDiff = generateList();
-    console.log("Number of Counties: " + listAsDiff.length);
-    game(listAsDiff, used);
-  }, 1000);
+  gameIsRunnig = true;
+  let point = 0;
+  let used = [];
+  
+  let listAsDiff = generateList();
+  console.log("Number of Counties: " + listAsDiff.length);
+  game(listAsDiff, used, 0);
 }
 
 // actual game
-async function game(listAsDiff) {
+async function game(listAsDiff, used, points) {
   if (used.length === listAsDiff.length) {
-    used = [];
-    console.error("RAN OUT OF COUNTRIES");
+    console.log("Progres: " + points + " / " + used.length);
+    console.error("NO COUNTRIES LEFT");
+    gameIsRunnig = false;
     return;
   }
-
   let rand = Math.floor(Math.random() * listAsDiff.length);
-
+  
   while (used.includes(listAsDiff[rand])) {
     rand = Math.floor(Math.random() * listAsDiff.length);
   }
-  used.push(listAsDiff[rand]);
-  secretField.innerText = listAsDiff[rand].name;
+  country = listAsDiff[rand];
+  used.push(country);
+  secretField.innerText = country.name;
 
   let guess = await getGuess();
 
   //coloring
-  if (evalGuess(guess, listAsDiff[rand])) {
-    led.style.backgroundColor = "#2DC943";
-    console.log("==> GOOD GUESS");
+  if (evalGuess(guess, country)) {
+    console.log("===> GOOD GUESS");
+    points++;
+    led.style.backgroundColor = '#4ADE80'
   } else {
-    led.style.backgroundColor = "#FE5F57";
-    console.log("==> BAD GUESS");
+    console.log("===> BAD GUESS");
+    led.style.backgroundColor = '#EF4444'
   }
 
   // synch timing of led and new secretCountry/capital field
   setTimeout(() => {
     led.style.backgroundColor = "transparent";
     guessField.value = "";
-    console.log(used.length);
-    game(listAsDiff, used);
+    console.log("Progres: " + points + " / " + used.length);
+
+    if (gameIsRunnig === true) {
+      game(listAsDiff, used, points);
+    }
+    else {
+      return;
+    }
   }, 900);
 }
 
@@ -174,9 +188,6 @@ function getGuess() {
 }
 
 function evalGuess(guess, randomCountry) {
-  console.log("GUESS:  " + guessField.value);
-  console.log("ANSWER: " + randomCountry.capital + "\n");
-
   if (guess === randomCountry.capital) {
     return true;
   } else {
